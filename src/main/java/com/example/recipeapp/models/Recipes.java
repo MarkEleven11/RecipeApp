b6691cb314ecb02842b5b3625cec19f737613d97
Александр <alexakr1993@gmail.com>
2023-03-21 11:56:46 +0400

    Done

--- src/main/java/com/example/recipeapp/models/Recipes.java
+++ src/main/java/com/example/recipeapp/models/Recipes.java
@@ -2,12 +2,14 @@ package com.example.recipeapp.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 @AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 public class Recipes {
 
     private String title;
